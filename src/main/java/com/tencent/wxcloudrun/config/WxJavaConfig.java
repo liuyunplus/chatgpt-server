d041c4dc42fc28e57b79c177c1a936f0a8fb0d4b
liuyun <liuyunplus@gmail.com>
2023-03-25 16:57:17 +0800

    fix

---
+++ src/main/java/com/tencent/wxcloudrun/config/WxJavaConfig.java
@@ -0,0 +1,29 @@
+package com.tencent.wxcloudrun.config;
+
+import com.tencent.wxcloudrun.service.TextHandler;
+import me.chanjar.weixin.common.api.WxConsts;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WxJavaConfig {
+
+    @Autowired
+    private WxMpService wxMpService;
+
+    @Autowired
+    private TextHandler textHandler;
+
+    @Bean
+    public WxMpMessageRouter messageRouter() {
+        // 创建消息路由
+        final WxMpMessageRouter router = new WxMpMessageRouter(wxMpService);
+        // 添加文本消息路由
+        router.rule().async(false).msgType(WxConsts.XmlMsgType.TEXT).handler(textHandler).end();
+        return router;
+    }
+
+}
--- src/main/java/com/tencent/wxcloudrun/controller/WeChatController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/WeChatController.java
@@ -2,29 +2,53 @@ package com.tencent.wxcloudrun.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tencent.wxcloudrun.config.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-import java.util.Random;
 
 /**
  * counter控制器
  */
+@Slf4j
 @RestController
 public class WeChatController {
 
-    @PostMapping(value = "/api/msg")
-    String get(@RequestBody Map<String, Object> param) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonString = objectMapper.writeValueAsString(param);
-        System.out.println(jsonString);
-        param.put("CreateTime", System.currentTimeMillis() / 1000);
-        param.put("Content", "欢迎来到我的博客");
-        param.remove("MsgId");
-        return objectMapper.writeValueAsString(param);
+    @Autowired
+    private WxMpService wxMpService;
+    @Autowired
+    private WxMpMessageRouter wxMpMessageRouter;
+
+    @PostMapping(value = "message", produces = "application/xml; charset=UTF-8")
+    public String handleMessage(@RequestBody String requestBody,
+                                @RequestParam("signature") String signature,
+                                @RequestParam("timestamp") String timestamp,
+                                @RequestParam("nonce") String nonce) {
+        log.info("handleMessage调用: " + requestBody);
+        // 校验消息是否来自微信
+        if (!wxMpService.checkSignature(timestamp, nonce, signature)) {
+            throw new IllegalArgumentException("非法请求，可能属于伪造的请求！");
+        }
+        // 解析消息体，封装为对象
+        WxMpXmlMessage inMessage = WxMpXmlMessage.fromXml(requestBody);
+        WxMpXmlOutMessage outMessage;
+        try {
+            // 将消息路由给对应的处理器，获取响应
+            outMessage = wxMpMessageRouter.route(inMessage);
+        } catch (Exception e) {
+            log.error("微信消息路由异常", e);
+            outMessage = null;
+        }
+        // 将响应消息转换为xml格式返回
+        return outMessage == null ? "" : outMessage.toXml();
     }
 
 }
