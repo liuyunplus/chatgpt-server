97bed8e02e3ab7dcff6bfe6a636ab02f4b37ce21
liuyun <liuyunplus@gmail.com>
2023-03-25 15:11:11 +0800

    fix

--- src/main/java/com/tencent/wxcloudrun/controller/WeChatController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/WeChatController.java
@@ -3,7 +3,7 @@ package com.tencent.wxcloudrun.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tencent.wxcloudrun.config.ApiResponse;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
  * counter控制器
  */
 @RestController
-
 public class WeChatController {
 
-    @GetMapping(value = "/api/msg")
+    @PostMapping(value = "/api/msg")
     ApiResponse get(@RequestBody Object param) throws JsonProcessingException {
       ObjectMapper objectMapper = new ObjectMapper();
       String jsonString = objectMapper.writeValueAsString(param);
